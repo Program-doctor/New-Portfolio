0bc7d1bd7e6e5537ce810add614e8cfef5f0862d
PromiseSyl <sylvesterpro2017@gmail.com>
2025-07-23 09:46:06 +0100

    Google Analytics
    Added g-tags to track activities and interactions of users in the website

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,7 +10,8 @@ export default function Portfolio(){
       id:1,
       image: Moodulo,
       title:'Moodulo - Productivity Web App',
-      details:'Moodulo is a smart, user-focused productivity tool design to help users organize tasks, track moods and improve daily routines. ',
+      details:'Moodulo is a smart, user-focused productivity tool design to help users organize tasks, track moods and improve daily routines.',
+      stacks: 'React • Tailwind CSS •JSON Server •',
       github:'https://github.com/Program-doctor/Moodulo',
       demo:'https://moodulo.netlify.app/',alt:'Productivity App'
     },
@@ -18,7 +19,8 @@ export default function Portfolio(){
       id:2,
       image:landingpage,
       title:'Gallery Portfolio',
-      details:'This gallery portfolio website is designed to showcase my skills as a frontend developer and designer. It features a clean, modern layout with responsive design elements that adjust seamlessly across devices.',
+      details:'Portfolio-style gallery website showcasing image collections. It features a clean, modern layout with responsive design elements that adjust seamlessly across devices.',
+      stacks: 'JavaScript • HTML • CSS',
       github:'https://github.com/Program-doctor/PPBusiness',
       demo:'https://exquisite-mooncake-fdbe30.netlify.app/',
       alt:"Portfolio Landing Page"
@@ -27,7 +29,8 @@ export default function Portfolio(){
       id:3,
       image:weatherapp,
       title:'React Weather App',
-      details:'Built with React and external APIs, this app provides real-time weather data and showcases clean UI design with component-based structure.  ',
+      details:'Fetches real-time data using OpenWeatherMap API. A simple UI to check current conditions and location based forecasts.',
+      stacks: 'React • CSS • API Integration',
       github:'https://github.com/Program-doctor/weather-react-app',
       demo:'http://splendid-pavlova-2335ff.netlify.app/',
       alt:'React Weather App'
@@ -39,12 +42,13 @@ export default function Portfolio(){
           <h2>Portfolio</h2> 
           <div className="portfolio-container"> 
           { 
-          data.map(({id,image,title,details,github,demo,alt})=>{
+          data.map(({id,image,title,details,stacks,github,demo,alt})=>{
             return(
           <div key={id} className="content">
           <img src={image} alt={alt}  width={330} height={400} /> 
           <h3>{title}</h3>
           <small className="text-light details">{details}</small>
+          <p><strong className="text-light">{stacks}</strong></p>
           <div className="links">
           <a href={github} target="_blank" rel="noopener noreferrer" className="btn" title="Link to Github">Github</a>
           <a href={demo} target="_blank" rel="noopener noreferrer" className="btn btn-primary" title="Link to Live page">Live Demo</a>
